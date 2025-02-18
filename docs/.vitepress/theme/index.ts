9f0d44cf914bbb19b93976c0c4a64ee113e6a015
laine001 <40457081+laine001@users.noreply.github.com>
2025-02-18 12:42:43 +0800

    💥

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -10,7 +10,7 @@ import './my-style.scss'
 
 // import { NImage, NBackTop, NIcon, NTag, NTooltip } from 'naive-ui'
 // import { CafeOutline } from '@vicons/ionicons5'
-import pkg from 'naive-ui';
+import * as pkg from 'naive-ui';
 const { NImage, NBackTop, NIcon, NTag, NTooltip } = pkg;
 
 export default {
