f25b64cbb4a050a16209687fe32771e872ae3b62
laine001 <40457081+laine001@users.noreply.github.com>
2025-03-14 21:58:25 +0800

    🍑

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,7 +1,6 @@
 // https://vitepress.dev/guide/custom-theme
 import { h, defineComponent } from 'vue'
 import { inBrowser } from 'vitepress'
-// import type { Theme } from 'vitepress'
 import busuanzi from 'busuanzi.pure.js'
 import DefaultTheme from 'vitepress/theme'
 import Layout from './Layout.vue'
@@ -9,10 +8,8 @@ import MyTooltip from './components/my-tooltip/index.vue'
 import './style.css'
 import './my-style.scss'
 
-// import { NImage, NBackTop, NIcon, NTag, NTooltip } from 'naive-ui'
-// import { CafeOutline } from '@vicons/ionicons5'
 import * as pkg from 'naive-ui';
-const { NImage, NBackTop, NIcon, NTag, NTooltip, NDataTable } = pkg;
+const { NImage, NBackTop, NTooltip, NConfigProvider } = pkg;
 
 export default {
   extends: DefaultTheme,
@@ -24,12 +21,9 @@ export default {
       }
     }
     app.component('n-image', NImage)
-    // app.component('CafeOutline', CafeOutline)
     app.component('n-back-top', NBackTop)
-    app.component('n-icon', NIcon)
-    app.component('n-tag', NTag)
     app.component('n-tooltip', NTooltip)
-    app.component('n-data-table', NDataTable)
+    app.component('NConfigProvider', NConfigProvider)
     app.component('m-tooltip', MyTooltip)
   }
 } satisfies Theme
