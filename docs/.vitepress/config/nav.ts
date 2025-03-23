c44cf572e3b3c6736a111dccbfe81fd817be9a27
laine001 <40457081+laine001@users.noreply.github.com>
2025-03-23 14:58:03 +0800

    🍼

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -12,7 +12,7 @@ export default [
     items: [
       {
         text: '一些名词概念',
-        link: '/summary/calc-concept'
+        link: '/summary/concept'
       },
       {
         text: '132个工具与技术',
